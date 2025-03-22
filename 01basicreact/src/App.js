768f242481077dafe29c2dac5882ca1988409e4b
Basavaraja-K <basavaraja_k@hermanmiller.com>
2025-03-22 19:08:46 +0530

    Initial Commit Changes regarding React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+ 
+function App() {
+  return (
+     <h1>Chai aur React | Basavaraja Learning React.</h1>
+  );
+}
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Chai aur Vite | Basavaraja Learning Vite </h1>    
+  )
+}
+
+export default App
